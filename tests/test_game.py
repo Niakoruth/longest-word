a9955f9a2c606d6c54cd78101bffd9c052767395
Nia Koruth <nia.a.koruth@gmail.com>
2025-02-20 19:13:32 +0000

    updating the test cases in test_game file

--- tests/test_game.py
+++ tests/test_game.py
@@ -15,29 +15,33 @@ class TestGame:
         for letter in grid:
             assert letter in string.ascii_uppercase
 
-    def test_empty_is_invalid(self):
-        new = Game()
-        test = []
-        assert new.is_valid(test) is False
+    def test_empty_word_is_invalid(self):
+        # setup
+        new_game = Game()
+        # verify
+        assert new_game.is_valid('') is False
+
 
     def test_is_valid(self):
-        new = Game()
-        test_grid = "POIUYTREW"
-        test_word = "POETRY"
-
-        new.grid = list(test_grid) # calling class object
-
-        #test scenarios
-        assert new.is_valid(test_word) is True
-        assert new.grid == list(test_grid) # making sure the grid is unchanged
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'EUREKA'
+        # exercise
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is True
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
 
     def test_is_invalid(self):
-        new = Game()
-        test_grid = "POIUYTREW"
-        test_word = "EUREKA"
-
-        new.grid = list(test_grid) # calling class object
-
-        #test scenarios
-        assert new.is_valid(test_word) is False
-        assert new.grid == list(test_grid) # making sure the grid is unchanged
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'SANDWICH'
+        # exercise
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is False
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
